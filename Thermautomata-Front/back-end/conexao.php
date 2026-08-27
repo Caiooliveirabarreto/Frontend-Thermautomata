f34c0a4e68b0152b9292fd1bcf3feb2f8bcdc285
ArthurLuz28 <arthurluz728@gmail.com>
2026-08-27 08:43:30 -0300

    Terminado as páginas de Entrar e Cadastrar - Começando back do login

---
+++ Thermautomata-Front/back-end/conexao.php
@@ -0,0 +1,15 @@
+<?php
+    $host = 'localhost';
+    $dbname = 'thermabd';
+    $user = 'root';
+    $pass = '';
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+        $pdo->exec("SET time_zone = '-03:00'");
+        // Habilita erros do PDO
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo "Conexão bem-sucedida!";
+    } catch (PDOException $e) {
+        echo "Erro na conexão: " . $e->getMessage();
+    }
+?>
---
+++ Thermautomata-Front/back-end/contar.php
@@ -0,0 +1,60 @@
+<?php
+    session_start();
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $nome = $_POST['nome'];
+        $senha = $_POST['senha'];
+        $tipo = $_POST['tipo'];
+        $email = $_POST['email'];
+
+        // // Remove caracteres não numéricos (caso venham por algum motivo)
+        // $cpf = preg_replace('/[^0-9]/', '', $cpf);
+
+        // // Verifica se o CPF tem 11 dígitos
+        // if (strlen($cpf) !== 11) {
+        //     die("O CPF deve conter exatamente 11 dígitos.");
+        // }
+
+        // // Função para validar o CPF
+        // function validarCPF($cpf) {
+        //     // Elimina CPFs inválidos conhecidos
+        //     if (preg_match('/(\d)\1{10}/', $cpf)) {
+        //         return false;
+        //     }
+
+        //     // Calcula os dígitos verificadores
+        //     for ($t = 9; $t < 11; $t++) {
+        //         $d = 0;
+        //         for ($c = 0; $c < $t; $c++) {
+        //             $d += $cpf[$c] * (($t + 1) - $c);
+        //         }
+        //         $d = ((10 * $d) % 11) % 10;
+        //         if ($cpf[$c] != $d) {
+        //             return false;
+        //         }
+        //     }
+
+        //     return true;
+        // }
+
+        // // Valida o CPF
+        // if (!validarCPF($cpf)) {
+        //     die("O CPF informado é inválido.");
+        // }
+
+        // // Guarda dados temporariamente na sessão
+        $_SESSION['dados_cadastro'] = [
+            'cpf' => $cpf,
+            'nome' => $nome,
+            'original' => $senha,
+            'senha' => password_hash($senha, PASSWORD_DEFAULT),
+            'tipo' => $tipo,
+            'email' => $email
+            
+
+        ];
+
+        // Redireciona para a página de cadastro final
+        header("Location: cadastro_final.php");
+        exit;
+    }
+?>
